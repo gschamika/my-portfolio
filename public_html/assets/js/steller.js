ef51bcd19ebce6d1684b3f4140565c406b426e35
gschamika <97242725+gschamika@users.noreply.github.com>
2024-02-23 18:20:39 +0530

    email contact

--- public_html/assets/js/steller.js
+++ public_html/assets/js/steller.js
@@ -1,17 +1,3 @@
-/*!
-=========================================================
-* Steller Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // smooth scroll
 $(document).ready(function(){
 	$(".nav-link").on('click', function(event) {
